8ebc4c83933926506fba8918b0ea6ed22ca9dae8
Rex Lee(李俊) <root@duguying.net>
2024-09-14 13:14:53 +0800

    support sections

--- d2.go
+++ d2.go
@@ -1,21 +1,25 @@
 package d2
 
 import (
-	"github.com/gogather/safemap"
+	"github.com/gogather/safemap/v2"
 )
 
 type D2 struct {
-	MD2 *safemap.SafeMap `json:"_d2"`
+	md2 *safemap.SafeMap
 }
 
 func NewD2() *D2 {
 	return &D2{
-		MD2: safemap.New(),
+		md2: safemap.New(),
 	}
 }
 
+func (md2 *D2) Sections() []string {
+	return md2.md2.Keys()
+}
+
 func (md2 *D2) Get(section, key string) (value interface{}, exist bool) {
-	sect, ok := md2.MD2.Get(section)
+	sect, ok := md2.md2.Get(section)
 	if !ok {
 		return nil, ok
 	}
@@ -24,7 +28,7 @@ func (md2 *D2) Get(section, key string) (value interface{}, exist bool) {
 }
 
 func (md2 *D2) GetSection(section string) (value *safemap.SafeMap, exist bool) {
-	sect, ok := md2.MD2.Get(section)
+	sect, ok := md2.md2.Get(section)
 	if !ok {
 		return nil, ok
 	}
@@ -33,7 +37,7 @@ func (md2 *D2) GetSection(section string) (value *safemap.SafeMap, exist bool) {
 }
 
 func (md2 *D2) Add(section, key string, value interface{}) {
-	sect, ok := md2.MD2.Get(section)
+	sect, ok := md2.md2.Get(section)
 	if !ok {
 		sect = safemap.New()
 	}
@@ -42,11 +46,11 @@ func (md2 *D2) Add(section, key string, value interface{}) {
 
 	sectMap.Put(key, value)
 
-	md2.MD2.Put(section, sectMap)
+	md2.md2.Put(section, sectMap)
 }
 
 func (md2 *D2) RemoveKey(section, key string) {
-	sectMap, ok := md2.MD2.Get(section)
+	sectMap, ok := md2.md2.Get(section)
 	if !ok {
 		return
 	}
@@ -56,23 +60,23 @@ func (md2 *D2) RemoveKey(section, key string) {
 		sect.Remove(key)
 	}
 	if len(sect.GetMap()) <= 0 {
-		md2.MD2.Remove(section)
+		md2.md2.Remove(section)
 	} else {
-		md2.MD2.Put(section, sect)
+		md2.md2.Put(section, sect)
 	}
 }
 
 func (md2 *D2) RemoveSection(section string) {
-	md2.MD2.Remove(section)
+	md2.md2.Remove(section)
 }
 
 func (md2 *D2) Clear() {
-	md2.MD2 = safemap.New()
+	md2.md2 = safemap.New()
 }
 
 func (md2 *D2) GetMapMap() map[string]map[string]interface{} {
 	dumpResult := map[string]map[string]interface{}{}
-	for section, lowerMap := range md2.MD2.GetMap() {
+	for section, lowerMap := range md2.md2.GetMap() {
 		sectionMap := map[string]interface{}{}
 		for key, value := range lowerMap.(*safemap.SafeMap).GetMap() {
 			sectionMap[key] = value
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
-module github.com/gogather/d2
+module github.com/gogather/d2/v2
 
 go 1.20
 
-require github.com/gogather/safemap v0.0.0-20170930074128-e4dc79c94fb6
+require github.com/gogather/safemap/v2 v2.0.0
 
 require github.com/gogather/com v1.0.3 // indirect
